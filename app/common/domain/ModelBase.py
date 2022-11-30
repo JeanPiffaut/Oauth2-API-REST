6dbb69011313fd85564d6648a3ae60ed8c435088
JeanPiffaut <piffaut.jean@gmail.com>
2022-11-30 20:10:13 -0300

    new model base

--- app/common/domain/ModelBase.py
+++ app/common/domain/ModelBase.py
@@ -1,2 +1,21 @@
+from typing import List
+
+
 class ModelBase:
-    pass
+    _errors: List = []
+
+    @property
+    def errors(self):
+        return self._errors
+
+    @errors.setter
+    def errors(self, error: List):
+        self._errors = error
+
+    @property
+    def error(self):
+        return self.errors
+
+    @error.setter
+    def error(self, val):
+        self._errors.append(val)
