7a385e0768d60076b1c4700f234df2ad11db4c16
JeanPiffaut <piffaut.jean@gmail.com>
2022-12-04 18:54:28 -0300

    modificando estructura

--- app/auth_types/domain/AuthTypeStructure.py
+++ app/auth_types/domain/AuthTypeStructure.py
@@ -1,3 +1,5 @@
+from flask import abort
+
 from app.auth_types.domain.AuthTypeClientId import AuthTypeClientId
 from app.auth_types.domain.AuthTypeClientSecret import AuthTypeClientSecret
 from app.auth_types.domain.AuthTypeId import AuthTypeId
@@ -13,15 +15,23 @@ class AuthTypeStructure(ModuleModel):
 
     def setId(self, auth_type_id):
         self._id = AuthTypeId(auth_type_id)
+        if self._id.is_valid() is False:
+            abort(400)
 
     def setName(self, auth_type_name):
         self._name = AuthTypeName(auth_type_name)
+        if self._name.is_valid() is False:
+            abort(400)
 
     def setClientId(self, auth_type_client_id):
         self._client_id = AuthTypeClientId(auth_type_client_id)
+        if self._client_id.is_valid() is False:
+            abort(400)
 
     def setClientSecret(self, auth_type_client_secret):
         self._client_secret = AuthTypeClientSecret(auth_type_client_secret)
+        if self._client_secret.is_valid() is False:
+            abort(400)
 
     def getId(self):
         return self._id.value
