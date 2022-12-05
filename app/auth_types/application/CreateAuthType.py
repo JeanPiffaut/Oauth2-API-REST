aed93accd4a38d1798431551bb257bc3f81c9460
JeanPiffaut <piffaut.jean@gmail.com>
2022-12-05 00:37:53 -0300

    Create authType

--- app/auth_types/application/CreateAuthType.py
+++ app/auth_types/application/CreateAuthType.py
@@ -1,7 +1,9 @@
 from app.auth_types.domain.AuthTypeStructure import AuthTypeStructure
+from app.auth_types.interface.FirestoreRepository import AuthTypeRepository
 
 
 class CreateAuthType(AuthTypeStructure):
 
     def execute(self):
-        pass
+        repo = AuthTypeRepository()
+        return repo.createAuthType(self.to_dict())
