22c4a0dbbcf2521291641f60a04c9c7e88237aa1
JeanPiffaut <piffaut.jean@gmail.com>
2022-12-05 02:34:48 -0300

    mostrar credenciales

--- app/credentials/application/ShowCredentials.py
+++ app/credentials/application/ShowCredentials.py
@@ -1,3 +1,7 @@
+from flask import abort
+
+from app.credentials.domain.Credential import Credential
+from app.credentials.domain.CredentialId import CredentialId
 from app.credentials.domain.CredentialStructure import CredentialStructure
 from app.credentials.interface.FirestoreRepository import CredentialRepository
 
@@ -6,3 +10,23 @@ class ShowCredentials(CredentialStructure):
 
     def execute(self, fill_id=None):
         repo = CredentialRepository()
+        credentials = []
+        if fill_id is not None:
+            credential_id = CredentialId(fill_id)
+            if credential_id.is_valid() is False:
+                abort(400)
+
+            result = repo.listCredentialsById(credential_id.value)
+            if result.exists:
+                credential = Credential()
+                credential.from_firestore_document(result)
+                credentials.append(credential.to_dict())
+        else:
+            result = repo.listCredentials(self.getUserId(), self.getAuthTypeId(), self.getUsername(), self.getToken())
+            for doc in result:
+                if doc.exists:
+                    credential = Credential()
+                    credential.from_firestore_document(doc)
+                    credentials.append(credential.to_dict())
+
+        return credentials
--- app/credentials/domain/Credential.py
+++ app/credentials/domain/Credential.py
@@ -2,4 +2,9 @@ from app.credentials.domain.CredentialStructure import CredentialStructure
 
 
 class Credential(CredentialStructure):
-    pass
+    def from_firestore_document(self, document):
+        self.setId(document.id)
+        self.setUserId(document.get('user_id'))
+        self.setAuthTypeId(document.get('auth_type_id'))
+        self.setUsername(document.get('username'))
+        self.setToken(document.get('token'))
