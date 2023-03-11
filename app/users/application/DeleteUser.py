958f57029acc4c06d44eeec2a0270afed575771f
JeanPiffaut <piffaut.jean@gmail.com>
2023-03-11 00:15:13 -0300

    comentarios

--- app/users/application/DeleteUser.py
+++ app/users/application/DeleteUser.py
@@ -8,14 +8,17 @@ from app.sessions.interface.FirestoreRepository import SessionRepository
 
 class DeleteUser(UserStructure):
     def execute(self, fill_id):
+        # validate id
         user_id = UserId(fill_id)
         if user_id.is_valid() is False:
             abort(400)
 
+        # delete session
         session_repository = SessionRepository()
         result = session_repository.delete_by_user_id(user_id.value)
 
         if result:
+            # delete user
             repo = UserRepository()
             return repo.deleteUser(user_id.value)
         else:
