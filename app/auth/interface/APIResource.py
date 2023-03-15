7124ff9845c604c2e83e7ccec399943d72aec3f8
JeanPiffaut <piffaut.jean@gmai.com>
2023-03-15 01:16:08 -0300

    metodo post para validar usuario

--- app/auth/interface/APIResource.py
+++ app/auth/interface/APIResource.py
@@ -1,14 +1,17 @@
 from flask import request
 from flask_restful import Resource
 
+from app import response_structure
 from app.common.interface.Miscellaneous import validate_headers
 
 
 class APIResource(Resource):
     def get(self):
         valid = validate_headers(request.headers)
-        if valid != True:
-            print(2)
+        if not valid:
             return valid
 
-        return {"message": "Hello World"}
+        return response_structure(404)
+
+    def post(self):
+        pass
