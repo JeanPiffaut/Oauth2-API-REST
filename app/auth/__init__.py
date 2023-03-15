165ce02c3bdb2cc9f6951558b6d1d968adc2019f
JeanPiffaut <piffaut.jean@gmai.com>
2023-03-15 03:09:22 -0300

    redireccion a google

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 
 from app import ExtendAPI
 from app.auth.interface.APIResource import APIResource
+from app.auth.interface.GoogleResource import GoogleResource
 from app.auth.interface.HTMLResource import HTMLResource
 
 auth_bp = Blueprint('auth', __name__)
@@ -10,3 +11,4 @@ api = ExtendAPI(auth_bp)
 # Add endpoints
 api.add_resource(HTMLResource, '/', endpoint='auth_resource')
 api.add_resource(APIResource, '/v1/login', endpoint='login_resource')
+api.add_resource(GoogleResource, '/v1/oauth2/google', endpoint='google_resource')
